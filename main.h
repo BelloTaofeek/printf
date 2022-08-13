f066edb7eee8c5903dbf0b351765c27f49a68a95
Iamsteveameh <atagastephen2@gmail.co>
2022-08-12 18:50:02 -0700

    Team Project

--- main.h
+++ main.h
@@ -68,7 +68,7 @@ int print_hexa_upper(va_list types, char buffer[],
 		     int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
-	       char buffer[], int flags, char flag_ch, int width, int precision, int size);
+	char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
